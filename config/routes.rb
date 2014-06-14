14aab8cd9316787af6e8bd6a296933bbeefbe3a4
Ted Hanson <ted.hanson@moovweb.com>
2014-06-14 12:14:57 -0700

    add routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  match '/auth/:provider/callback' => 'sessions#create'
+
+  match '/signout' => 'sessions#destroy', :as => :signout
+
+  match '/signin' => 'sessions#new', :as => :signin
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
