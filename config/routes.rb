1c319239b45a649baca23bd2acc17013e93fe804
Ted Hanson <ted.hanson@moovweb.com>
2014-06-14 13:45:53 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get '/auth/:provider/callback' => 'sessions#create'
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]  
 
-  get '/signout' => 'sessions#destroy', :as => :signout
-
-  get '/signin' => 'sessions#new', :as => :signin
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/migrate/20140614204312_create_users.rb
+++ db/migrate/20140614204312_create_users.rb
@@ -4,7 +4,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :provider
       t.string :uid
       t.string :name
-      t.string :email
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
 
       t.timestamps
     end
