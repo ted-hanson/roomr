2839b20a7f53a223f51d847961b6d323113c4999
Ted Hanson <ted.hanson@moovweb.com>
2014-06-14 15:39:45 -0700

    logging in redirects you to current user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]  
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
