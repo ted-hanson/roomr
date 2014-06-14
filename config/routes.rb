49e4d67dc94628d9d7848e0ebd693f353f5e6910
Honie Lui <honie.lui@moovweb.com>
2014-06-14 16:16:36 -0700

    can add listing to the db, super basic view done for listing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]  
+  resources :listings
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
