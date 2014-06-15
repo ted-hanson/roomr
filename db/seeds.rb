64360cf01f7d02dbdb5843763a804b4846f8c6ae
Mattmlm <matthew.l.mo@gmail.com>
2014-06-15 13:40:43 -0700

    Seeds - fixing seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,55 +8,6 @@
 
 time1 = Time.new
 
-/haloUsers = User.create([
-	{
-		provider: "",
-		uid: "cortana",
-		first_name: "Cortana",
-		last_name: "CTN 0452-9",
-		email: "Cortana@halo.net",
-		birthday_month: "11",
-		birthday_year: "2549",
-		gender: "female",
-		photo: "http://i.imgur.com/pxdiHL8.jpg",
-		occupation: "Military AI",
-		interests: "multitasking, keeping John-117 alive",
-		oauth_token: "",
-		oauth_expires_at: Time.new + 2000
-	},
-	{
-		provider: "",
-		uid: "masterchief",
-		first_name: "John",
-		last_name: "117",
-		email: "john117@halo.net",
-		birthday_month: "3",
-		birthday_year: "2511",
-		gender: "male",
-		photo: "http://i.imgur.com/UVrqIax.jpg",
-		occupation: "Soldier",
-		interests: "Knifing people with an energy sword",
-		oauth_token: "",
-		oauth_expires_at: Time.new + 2000
-	}
-])
-metroidUser = User.create([
-	{
-		provider: "",
-		uid: "samusaran",
-		first_name: "Samus",
-		last_name: "Aran",
-		email: "samusaran@powership.net",
-		birthday_month: "8",
-		birthday_year: "1986",
-		gender: "female",
-		photo: "http://i.imgur.com/fR5nz9V.jpg?1",
-		occupation: "Bounty Hunter",
-		interests: "Hunting space pirates",
-		oauth_token: "",
-		oauth_expires_at: Time.new + 2000
-	}
-])/
 sfUsers = User.create([
 	{
     provider: "",
@@ -207,79 +158,24 @@ sfUsers = User.create([
     interests: "crime solving, social service, action",
     oauth_token: "",
     oauth_expires_at: Time.new + 2000
-  }
-])
-/haloListing = Listing.create([
-	{
-		title: "Bunker",
-		description: "Military, cozy, ",
-		neighborhood: "Spaceship",
-		dateavailable_month: time1.month,
-		dateavailable_day: time1.day,
-		dateavailable_year: time1.year,
-		numbedrooms: 1,
-		numbathrooms: 0,
-		price: 100000000000,
-		photos: "http://i.imgur.com/kqgJ525.jpg?1",
-		user_id: 2
+  },
+  {
+		provider: "",
+		uid: "nancydrew",
+		first_name: "Nancy",
+		last_name: "Drew",
+		email: "nancydrew@nancymystery.com",
+		birthday_month: "9",
+		birthday_year: "1989",
+		gender: "femal",
+		photo: "http://media.npr.org/programs/morning/features/2008/jun/nancy_drew/nancydrew200-056dd44a1ab1a028a753e15bf9a1187e111b1a42-s6-c30.jpg",
+		occupation: "Girl Detective",
+		interests: "Crime solving, reading, travel, hardy",
+		oauth_token: "",
+		oauth_expires_at: Time.new + 2000
 	}
 ])
-metroidListing = Listing.create([
-	{
-		title: "Bunker",
-		description: "Military cozy",
-		neighborhood: "Gunship",
-		dateavailable_month: time1.month,
-		dateavailable_day: time1.day,
-		dateavailable_year: time1.year,
-		numbedrooms: 1,
-		numbathrooms: 1,
-		price: 100000000000,
-		photos: "http://i.imgur.com/vIYlucZ.jpg",
-		user_id: 3
-	}
-])
-marioListing = Listing.create([
-	{
-		title: "Mushroom Kingdom Castle",
-		description: "",
-		neighborhood: "Palace of Fine Arts",
-		dateavailable_month: time1.month,
-		dateavailable_day: time1.day,
-		dateavailable_year: time1.year,
-		numbedrooms: 1,
-		numbathrooms: 0,
-		price: 4000,
-		photos: "http://i1186.photobucket.com/albums/z363/PancakeLake/dm_peachs_castle0000.jpg",
-		user_id: 6
-	},
-	{
-		title: "Plumber Apt",
-		description: "A little flat",
-		neighborhood: "Little Italy, SF",
-		dateavailable_month: time1.month,
-		dateavailable_day: time1.day,
-		dateavailable_year: time1.year,
-		numbedrooms: 1,
-		numbathrooms: 0,
-		price: 2000,
-		photos: "http://static.giantbomb.com/uploads/scale_small/1/17172/1099941-marioshouse.jpg",
-		user_id: 4
-	},
-	{
-		title: "Bowser's Castle",
-		description: "Spiky",
-		neighborhood: "Candlestick Park, SF",
-		dateavailable_month: time1.month,
-		dateavailable_day: time1.day,
-		dateavailable_year: time1.year,
-		numbedrooms: 1,
-		numbathrooms: 0,
-		price: 100000000000,
-		photos: "http://img4.wikia.nocookie.net/__cb20120313140737/mcleodgaming/images/d/df/Bowser%27s_castle.png",
-		user_id: 7
-	}
-])/
+
 sfListing = Listing.create([
 	{
     title: "Be my roommate to my awesome 4bd house",
@@ -290,6 +186,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 4,
     numbathrooms: 2,
+    match_grade: 'A',
     price: 1800,
     photos: "http://i.imgur.com/MDy6eqe.png",
     user_id: 1
@@ -303,6 +200,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 3,
     numbathrooms: 1,
+    match_grade: 'B',
     price: 1200,
     photos: "http://i.imgur.com/MDy6eqe.png",
     user_id: 2
@@ -316,6 +214,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 2,
     numbathrooms: 1,
+    match_grade: 'A+',
     price: 600,
     photos: "http://i.imgur.com/gfM3U6d.jpg",
     user_id: 3
@@ -329,6 +228,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 3,
     numbathrooms: 1,
+    match_grade: 'C',
     price: 800,
     photos: "http://i.imgur.com/wka2fwy.jpg",
     user_id: 4
@@ -342,6 +242,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 1,
     numbathrooms: 0,
+    match_grade: 'D',
     price: 800,
     photos: "http://i.imgur.com/y5BLHrs.png",
     user_id: 5
@@ -355,6 +256,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 1,
     numbathrooms: 0,
+    match_grade: 'C',
     price: 2000,
     photos: "http://i.imgur.com/QslbXzl.jpg",
     user_id: 6
@@ -368,6 +270,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 1,
     numbathrooms: 0,
+    match_grade: 'F',
     price: 40000,
     photos: "http://i.imgur.com/SAuTcEX.jpg",
     user_id: 7
@@ -381,6 +284,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 1,
     numbathrooms: 0,
+    match_grade: 'F',
     price: 40000,
     photos: "http://i.imgur.com/KGNjFUl.jpg",
     user_id: 8
@@ -394,6 +298,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 3,
     numbathrooms: 1,
+    match_grade: 'F',
     price: 40000,
     photos: "http://i.imgur.com/KGNjFUl.jpg",
     user_id: 9
@@ -407,6 +312,7 @@ sfListing = Listing.create([
     dateavailable_year: time1.year,
     numbedrooms: 3,
     numbathrooms: 1,
+    match_grade: 'B',
     price: 2000,
     photos: "http://i.imgur.com/KGNjFUl.jpg",
     user_id: 10
