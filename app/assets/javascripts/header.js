3087757f3f010c91260915d31a27b6c5ffd02f74
worthf38 <worth.freeman@moovweb.com>
2014-06-15 14:32:48 -0700

    update users edit/ header

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,15 +1,16 @@
-$(function() {
-  $("#menu_nav_button").click(function() {
+$(document).on("page:load ready",function() {
+  $("#menu_nav_button").on("click",function() {
     $nav = $("#nav");
     $maincontent = $("div.main-content")
     if ($nav.is(".active")) {
       $nav.hide().removeClass("active")
       $maincontent.show()
+      $("body").removeClass("nav-open")
     }
     else {
       $nav.show().addClass("active")
       $maincontent.hide()
-
+      $("body").addClass("nav-open")
     }
   })
 })
