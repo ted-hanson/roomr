db43522e277be31fecfbf0d39cc330799d27d02a
Mattmlm <matthew.l.mo@gmail.com>
2014-06-14 18:41:25 -0700

    Users - adding index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+      respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @listings }
+    end
+  end
+
   def new
   end
   
